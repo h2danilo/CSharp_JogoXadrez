460709e4d9ee2fa6a0cb34f28a92aa5e62f45c53
h2danilo <h2danilo@hotmail.com>
2018-11-07 21:47:24 -0200

    Imprimindo peças por cor

--- JogoXadrez-Console/xadrez/PosicaoXadrez.cs
+++ JogoXadrez-Console/xadrez/PosicaoXadrez.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using tabuleiro;
 
-namespace JogoXadrez_Console.xadrez {
+namespace xadrez {
     class PosicaoXadrez {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao ToPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString() {
+            return ""+ coluna + linha;
+        }
+
     }
 }
